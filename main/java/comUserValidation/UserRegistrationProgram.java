30a229b09bfa7e1ed729662f68f6ad954d736f63
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-04-14 18:51:28 +0530

    ADD: Last name validated

--- main/java/comUserValidation/UserRegistrationProgram.java
+++ main/java/comUserValidation/UserRegistrationProgram.java
@@ -11,10 +11,19 @@ public class UserRegistrationProgram {
         return Pattern.matches("^[A-Z]{1}[a-z]{2,}", firstName);
     }
     
+    /*
+     * UC-2: validating last name
+     */
+    public static boolean validateLastName(String lastName) {
+        return Pattern.matches("^[A-Z]{1}[a-z]{2,}", lastName);
+    }
+    
     //main method
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Simulator!");
         String firstName = "Rahul";
+        String lastName = "Rahul";
         System.out.println("first name: " + validateFirstName(firstName));
+        System.out.println("last name: " + validateLastName(lastName));
     }
 }
