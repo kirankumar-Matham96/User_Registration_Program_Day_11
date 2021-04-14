e409364d13ecdb47d0f26d3f7c72e2b0548f1abb
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-04-14 18:55:31 +0530

    ADD: Email validated

--- main/java/comUserValidation/UserRegistrationProgram.java
+++ main/java/comUserValidation/UserRegistrationProgram.java
@@ -18,12 +18,21 @@ public class UserRegistrationProgram {
         return Pattern.matches("^[A-Z]{1}[a-z]{2,}", lastName);
     }
     
+    /*
+     * UC-3: validating email
+     */
+    public static boolean validateEmail(String email) {
+        return Pattern.matches("^(?!\\.)[A-Za-z0-9]+([._%+-]?[0-9])*@[A-Za-z0-9-]+\\.[a-zA-Z]{2,}(\\.[A-Za-z]{2,})?$", email);
+    }
+    
     //main method
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Simulator!");
         String firstName = "Rahul";
         String lastName = "Rahul";
+        String email = "rahul@gmail.com";
         System.out.println("first name: " + validateFirstName(firstName));
         System.out.println("last name: " + validateLastName(lastName));
+        System.out.println("email: " + validateEmail(email));
     }
 }
